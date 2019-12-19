0cc6017facf478f79301cfba043fbde850dd5e62
Yuri Pogodaev <pogodaev.yuri@gmail.com>
2019-12-19 11:26:04 +0300

    implement HW_2

---
+++ Lesson2/Person.java
@@ -0,0 +1,27 @@
+class Person {
+	String male = "man";
+	String name = "Yuri";
+	float height = 1.82f;
+	float weight = 89.1f;
+	int age = 29;
+
+	void move() {
+
+	}
+
+	void sit() {
+
+	}
+
+	void run() {
+
+	}
+
+	void talk() {
+
+	}
+
+	void learnJava() {
+
+	}
+}
