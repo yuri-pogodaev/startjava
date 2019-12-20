7b00c86afec6ba492e3422ff412b1d7e0b5ddbe6
Yuri Pogodaev <pogodaev.yuri@gmail.com>
2019-12-20 11:41:07 +0300

    test

---
+++ Lesson2/test.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+
+        while (true) {
+
+            System.out.println("Введите первое число: ");
+            Scanner scanFirstNumber = new Scanner(System.in);
+            int numberFirst = scanFirstNumber.nextInt();
+
+            System.out.println("Введите знак математической операции: ");
+            Scanner scanSign = new Scanner(System.in);
+            String sign = scanSign.nextLine();
+
+            System.out.println("Введите второе число: ");
+            Scanner scanSecondNumber = new Scanner(System.in);
+            int numberSecond = scanSecondNumber.nextInt();
+
+            System.out.println(sign + " " + numberFirst + " " + numberSecond);
+
+            String answer = "";
+            while (true) {
+                System.out.println("Будешь играть?");
+                Scanner scanAnswer = new Scanner(System.in);
+                answer = scanAnswer.nextLine();
+                if (answer.equals("нет") || answer.equals("да")) {
+                    break;
+                }
+            }
+            if (answer.equals("нет")) break;
+        }
+    }
+}
