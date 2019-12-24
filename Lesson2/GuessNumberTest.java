c6250cbd9d700ff25e129df91561386d97ff12bd
Yuri Pogodaev <pogodaev.yuri@gmail.com>
2019-12-24 14:09:57 +0300

    implement HW_2 task_7 ver1

--- Lesson2/GuessNumberTest.java
+++ Lesson2/GuessNumberTest.java
@@ -3,17 +3,17 @@ import java.util.Scanner;
 public class GuessNumberTest {
 	public static void main(String[] args) {
 		while (true) {
-		 	Scanner scanner = new Scanner(System.in);
-        	System.out.println("Введите имя первого парня ");
-        	String firstName = scanner.nextLine();
-        	Player player1 = new Player(firstName);
-        	System.out.println("Введите имя второго парня ");
-        	String secondName = scanner.nextLine();
-        	Player player2 = new Player(secondName);
-            
-        	GuessNumber guessNumber = new GuessNumber(player1, player2);
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Введите имя первого парня ");
+            String firstName = scanner.nextLine();
+            Player player1 = new Player(firstName);
+            System.out.println("Введите имя второго парня ");
+            String secondName = scanner.nextLine();
+            Player player2 = new Player(secondName);
+
+            GuessNumber guessNumber = new GuessNumber(player1, player2);
         
-        	guessNumber.startGame();
+            guessNumber.startGame();
 
 			String answer = "";
 			while (!(answer.equals("нет") || answer.equals("да"))) {
