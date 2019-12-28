a6158b3be382bef1abe26bae1eb0a919d445c4da
Yuri Pogodaev <pogodaev.yuri@gmail.com>
2019-12-28 19:57:39 +0300

    Lesson 4 Task2 ver6

--- src/com/startjava/lesson_4/game/Player.java
+++ src/com/startjava/lesson_4/game/Player.java
@@ -21,7 +21,6 @@ public class Player {
 
     public int[] getPuzzledNumbers() {
         return Arrays.copyOf(puzzledNumbers,counter);
-        //return puzzledNumbers;
     }
 
     public int getCurrentNumber() {
